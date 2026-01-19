0d3e63a2c8172beed58c0b62454c3ac613f47149
everybody-ah <fiorenicolas2005@gmail.com>
2026-01-18 23:49:59 -0500

    added graphs for each zip i love looking at graphs OMG

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import json
 import sqlite3
 import matplotlib.pyplot as plt 
-
+from graph import house_x_median
 from matplotlib.figure import Figure
 import base64
 from io import BytesIO
@@ -11,6 +11,20 @@ from flask import Flask, request, redirect, render_template
 
 app = Flask(__name__)
 
+ZIPCODES = ['15003', '15005', '15006', '15007', '15014', '15015', '15017', '15018', '15020', '15024', 
+            '15025', '15026', '15028', '15030', '15031', '15034', '15035', '15037', '15044', '15045', 
+            '15046', '15047', '15049', '15051', '15056', '15057', '15063', '15064', '15065', '15068', 
+            '15071', '15075', '15076', '15082', '15083', '15084', '15085', '15086', '15088', '15089', 
+            '15090', '15101', '15102', '15104', '15106', '15108', '15110', '15112', '15116', '15120', 
+            '15122', '15126', '15129', '15131', '15132', '15133', '15135', '15136', '15137', '15139', 
+            '15140', '15142', '15143', '15144', '15145', '15146', '15147', '15148', '15201', '15202', 
+            '15203', '15204', '15205', '15206', '15207', '15208', '15209', '15210', '15211', '15212', 
+            '15213', '15214', '15215', '15216', '15217', '15218', '15219', '15220', '15221', '15222', 
+            '15223', '15224', '15225', '15226', '15227', '15228', '15229', '15232', '15233', '15234', 
+            '15235', '15236', '15237', '15238', '15239', '15241', '15243', '15321', '15332', '15642', 
+            '15668', '16046', '16059', '16229']
+
+
 def get_db(): 
     con = sqlite3.connect("zipcodes.db")
     con.row_factory = sqlite3.Row
@@ -19,51 +33,42 @@ def get_db():
 
 with open ('house_prices.json') as j: 
     data_dict = json.load(j)
+    
+y= []
+for i in data_dict.values(): 
+    x = []
+    for j in i.values(): 
+        x.append(j['median_price'])
+    y.append(x)
+
 
 
 @app.route('/')
 def index(): 
-    con = get_db()
-    c = con.cursor()
-    rows = c.execute("SELECT DISTINCT(zipcode) FROM zipcodes").fetchall()
-    zipcodes = [row[0] for row in rows]
-    con.close()
-    return render_template('index.html',zipcodes=zipcodes)
+    return render_template('index.html',zipcodes=ZIPCODES)
 
 
 @app.route('/test', methods=['POST'])
 def graph():
+
+    #get zipcode
     zipcode = request.form.get('zipcode')
+    if zipcode not in ZIPCODES: 
+        return render_template('error.html')
+    
+    #connect db
     con = get_db()
     c = con.cursor()
-    rows = c.execute("SELECT median_price FROM zipcodes WHERE zipcode == ?", (zipcode,)).fetchall() 
+
+    #fetch houses_sold and median_price from entered zipcode
+    x1 = c.execute("SELECT houses_sold FROM zipcodes WHERE zipcode == ?", (zipcode,)).fetchall() 
+    x2 = c.execute("SELECT median_price FROM zipcodes WHERE zipcode == ?", (zipcode,)).fetchall() 
     con.close()
-    median_price = [row[0] for row in rows]
-    print(median_price)
-    return render_template('test.html', median_price=median_price)
 
+    #assign houses_sold to (x1) and median_price to (x2)
+    x1 = [i[0] for i in x1]
+    x2 = [j[0] for j in x2]
+    graph = house_x_median(x1, x2, zipcode)
 
-
-
-
-
-
-# zipcode = input("Enter Zip: ")
-
-
-# c.execute(query)
-# rows = c.fetchall()
-
-
-
-#114 zips per year
-
-
-# rows = [row[0] for row in rows]
-
-# x = np.arange(2000, 2026)
-# h = zip(x, rows)
-# print(tuple(h))
-# plt.bar(x, rows)
-# plt.xlabel("Years")
-# plt.show()
+    print(list(zip(x1, x2)))
+    return render_template('test.html', graph=graph)
